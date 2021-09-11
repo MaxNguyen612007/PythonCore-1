7863b1d76b360af88e996ae5c43fcc5c5a0a39de
MaxNguyen612007 <83494420+MaxNguyen612007@users.noreply.github.com>
2021-09-11 13:00:10 +1200

    Update code3.py

--- hw10/code3.py
+++ hw10/code3.py
@@ -1,4 +1,4 @@
-class Dog:
+ class Dog:
     species = 'mammal'
 
     def __init__(self, name, age):
@@ -6,40 +6,18 @@ class Dog:
         self.age = age
         self.is_hungry = True
 
-    def description(self):
-        return f'{self.name} is {self.age} years old'
-
-    def speak(self, sound):
-        return f'{self.name} says {sound}'
-
     def eat(self):
         if self.is_hungry:
             self.is_hungry = False
         else:
             print(f"{self.name} is not hungry!")
 
-class Pet:
-    def __init__(self,dogs):
-        self.dogs = dogs
-    def feed(self):
-        feeded = True
-        for dog in self.dogs:
-            if dog.is_hungry:
-                feeded = False
-                dog.eat()
-        
-        if feeded:
-            print("My dogs are not hungry.")
-            for dog in self.dogs:
-                dog.is_hungry = True
-        
-    def __str__(self):
-        s = ""
-        s += f"I have {len(self.dogs)} dogs\n"
-        for dog in self.dogs:
-            s += f"{dog.name} is {dog.age} years old. "
-        s += "\nand they are mammals, of course."
-        return s
+    def description(self):
+        return f'{self.name} is {self.age} years old'
+
+    def speak(self, sound):
+        return f'{self.name} says {sound}'
+
 
 class RussellTerrier(Dog):
     def run(self, speed):
@@ -50,13 +28,35 @@ class Bulldog(Dog):
     def run(self, speed):
         return f'{self.name} runs {speed}'
 
-my_pets = Pet([
-    Dog("Tom",6),
-    Dog("Jerry",9),
-    Dog("Butt",3),
-    Dog("Wine",1)
-])
-print(my_pets)
 
-my_pets.feed()
-my_pets.feed()
+class Pet:
+    def __init__(self, my_dogs):
+        self.my_dogs = my_dogs
+
+    def status(self):
+        have_eaten = True
+        print("My dogs are hungry.")
+        for dog in self.my_dogs:
+            if dog.is_hungry:
+                have_eaten = False
+                dog.eat()
+        
+        if have_eaten:
+            print("My dogs are not hungry.")
+            for dog in self.my_dogs:
+                dog.is_hungry = True
+    
+    def __str__(self):
+        dog_string = ""
+        dog_string += f"I have {len(self.my_dogs)} dogs. "
+        for dog in self.my_dogs:
+            dog_string += f"{dog.name} is {dog.age} years old. "
+        dog_string += "And they are mammals, of course."
+        return dog_string
+
+pets = Pet([Dog("Tom", 6), Dog("Jerry", 9), Dog("Butt", 3), Dog("Wine", 1)])
+print(pets)
+pets.status()
+
+
+
